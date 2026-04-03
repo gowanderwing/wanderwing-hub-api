817d335d1283bd85af1338bf6133c717faffd8c0
gowanderwing <gowanderwingtoday@gmail.com>
2026-04-03 07:41:18 -0400

    Modify handler to include query parameter 'id'

--- api/provider.js
+++ api/provider.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json({ ok: true, route: 'provider' });
 }
+export default function handler(req, res) {
+  const { id } = req.query;
+  res.status(200).json({ ok: true, id: id || null });
+}
